4b706fd1755f5bc9e23400476c2f5bcb197eb53f
Shawn Poulson <spoulson@entech.com>
2011-06-16 18:04:16 -0400

    Clean up unnecessary code.

--- Expl.Itinerary/Auxiliary/Calendar.cs
+++ Expl.Itinerary/Auxiliary/Calendar.cs
@@ -11,23 +11,6 @@ namespace Expl.Auxiliary {
       private static readonly int[] monthDaysLookup = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
       /// <summary>
-      /// Compute the start date of a given week of the month.
-      /// </summary>
-      /// <remarks>
-      /// TODO: Test after refactoring.
-      /// </remarks>
-      /// <param name="year">Year.</param>
-      /// <param name="month">Month.</param>
-      /// <param name="week">Week of the month.</param>
-      /// <returns>DateTime of Monday.</returns>
-      public static DateTime GetWeekStart(int year, int month, int week) {
-         var monthStartDate = new DateTime(year, month, 1);
-         var monthStartDOW = (int)monthStartDate.DayOfWeek;
-         int startDay = (week - 1) * 7 - monthStartDOW + 1;
-         return monthStartDate.AddDays(startDay);
-      }
-
-      /// <summary>
       /// Get number of days in a given month.
       /// </summary>
       /// <param name="year">Year.</param>
@@ -36,15 +19,6 @@ namespace Expl.Auxiliary {
       public static int GetMonthDays(int year, int month) {
          if (month == 2) return IsLeapYear(year) ? 29 : 28;
          else return monthDaysLookup[month - 1];
-      }
-
-      /// <summary>
-      /// Get number of days in a given month.
-      /// </summary>
-      /// <param name="date">Date value.</param>
-      /// <returns>Number of days</returns>
-      public static int GetMonthDays(this DateTime date) {
-         return GetMonthDays(date.Year, date.Month);
       }
 
       /// <summary>
@@ -57,15 +31,6 @@ namespace Expl.Auxiliary {
             (
                (year % 100 != 0) || (year % 400 == 0)
             );
-      }
-
-      /// <summary>
-      /// Get whether a given year is a leap year.
-      /// </summary>
-      /// <param name="date">Date value.</param>
-      /// <returns>True if leap year</returns>
-      public static bool IsLeapYear(this DateTime date) {
-         return IsLeapYear(date.Year);
       }
    }
 }
